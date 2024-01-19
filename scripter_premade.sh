aba52f917f43388cc4224815bae4906489ca3810
ThatLinuxFan <9904667+NodeMixaholic@users.noreply.github.com>
2024-01-19 15:51:11 -0600

    Stability Update II
    Make scripter easier and more stable by making the user input a text file instead of an (admitedly) easily broken string.

--- scripter_premade.sh
+++ scripter_premade.sh
@@ -1,4 +1,4 @@
-echo "$1" > script.txt
+cat "$1" > script.txt
 awk '{ print $2 }' script.txt > script-keywords.txt
 awk '{ print $4 }' script.txt >> script-keywords.txt
 awk '{ print $6 }' script.txt >> script-keywords.txt
