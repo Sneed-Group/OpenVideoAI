26cc3d7cce2df76dd1f88632dc559e7dcc670d1f
ThatLinuxFan <9904667+NodeMixaholic@users.noreply.github.com>
2024-01-19 14:09:20 -0600

    Add files via upload

--- keywords2images.sh
+++ keywords2images.sh
@@ -8,7 +8,9 @@ cd img/
 # Find and delete images not divisible by 2
 find . -type f -exec bash -c 'if [ $(identify -format "%h" "{}")%2 -ne 0 ]; then rm "{}"; fi' \;
 
+
+# Find files with 0 size
+find . -type f -size 0 -delete
+
 # Return to the original directory (optional)
 cd ..
-
-find img -type f -size 0 -delete
