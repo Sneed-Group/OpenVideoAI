cd89f092e3f104e385f7f3bd155476cbbba7fbd3
ThatLinuxFan <9904667+NodeMixaholic@users.noreply.github.com>
2024-01-19 22:22:55 +0000

    Update scripter_premade.sh

--- scripter_premade.sh
+++ scripter_premade.sh
@@ -1,4 +1,4 @@
-cat "$1" > script.txt
+mv "$1" script.txt
 awk '{ print $2 }' script.txt > script-keywords.txt
 awk '{ print $4 }' script.txt >> script-keywords.txt
 awk '{ print $6 }' script.txt >> script-keywords.txt
